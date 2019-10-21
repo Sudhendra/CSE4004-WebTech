1e09ae6829e91bdd0cacc57b0a31351d39c9b7b7
Sudhendra <sudhendrakambhamettu@gmail.com>
2019-10-21 23:47:47 +0530

    Update and rename test.java to print.java

--- servlet/print.java
+++ servlet/print.java
@@ -13,8 +13,8 @@ public class test extends HttpServlet{
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
-		int s1 = Integer.parseInt(req.getParameter("inp1"));
-		int s2 = Integer.parseInt(req.getParameter("inp2"));
+		int s1 = Integer.parseInt(req.getParameter("input1"));
+		int s2 = Integer.parseInt(req.getParameter("input2"));
 		int s= s1+s2;
 		 PrintWriter out = resp.getWriter();
 	        out.println(s);	}
